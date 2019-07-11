d57f7396fac3744644ece865ac061b2937da1937
dtrts <djt210@gmail.com>
2019-07-11 15:59:12 +0100

    This test was written before the method. Git is fake news

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,11 +1,11 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do 
+  it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq "fizz"
   end
 
-  it 'returns "buzz" when passed 5' do 
+  it 'returns "buzz" when passed 5' do
     expect(fizzbuzz(5)).to eq "buzz"
   end
 
@@ -17,20 +17,24 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(2)).to eq 2
   end
 
-  it 'returns "fizzbuzz" when passed 15' do 
+  it 'returns "fizzbuzz" when passed 15' do
     expect(fizzbuzz(15)).to eq "fizzbuzz"
   end
 
-  it 'returns "fizz" when passed 9' do 
+  it 'returns "fizz" when passed 9' do
     expect(fizzbuzz(9)).to eq "fizz"
   end
 
-  it 'returns "buzz" when passed 10' do 
+  it 'returns "buzz" when passed 10' do
     expect(fizzbuzz(10)).to eq "buzz"
   end
-  
+
   it 'returns ["fizz", "buzz", 11] when passed [3, 5, 11]' do
     expect(fizzbuzz([3, 5, 11])).to eq ["fizz", "buzz", 11]
   end
 
+
+  it 'modifies [1,2,3,5,15,6,10,[2,3]] to [1,2,"fizz","buzz","fizzbuzz","fizz","buzz",[2,"fizz"]]' do
+    expect([1,2,3,5,15,6,10,[2,3]].fizzbuzz!).to eq [1,2,"fizz","buzz","fizzbuzz","fizz","buzz",[2,"fizz"]]
+  end
 end
